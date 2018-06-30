2f035ccef97d84e9f07bf3701c452d6d200be3fc
Noel da Costa <noel@arc2.co.uk>
2018-06-30 23:40:22 +0100

    Added authorization fields

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,8 @@
     :url="url"
     multiple
     auto-expand
+    :header="header"
+    :additional-fields="additionalFields"
     >
 	</q-uploader>
   </q-page>
@@ -18,7 +20,15 @@ export default {
   name: 'PageIndex',
   data() {
   	return {
-  		url: 'http://localhost:8082/upload'
+  		// url: 'http://localhost:8082/upload'
+  		url: 'http://192.168.33.10:8080/api/upload_item_attachment/1',
+  		header: {
+  			'authorization': 'Bearer 410e18a7-5fd5-480d-9e0f-dba85928a94f'
+  		},
+  		additionalFields: [
+  			{token: '410e18a7-5fd5-480d-9e0f-dba85928a94f'}
+  		]
+  		// url: 'http://0.0.0.0:8082/upload'
   	}
   }
 }
